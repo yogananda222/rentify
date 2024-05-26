707d59469124efe76a9ab4488c9cbdc0f0fe7dd9
yogananda222 <yoganandanaidu14382@gmail.com>
2024-05-26 09:32:34 +0530

    initial commit

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const whitelist = ['https://rentify-5f5f7381b151.herokuapp.com'];
+const corsOptions = {
+    origin: function (origin, callback) {
+      if (whitelist.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+  };
+  
+app.use(cors(corsOptions));
 app.use(express.static(__dirname + '/dist/angular-17-e-commerce-app/browser'));
 app.get('/*', function(req,res)
 
